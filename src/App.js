39ffbc8410785d1d560e175b70d4122a1cf07ac8
Markus Johansen <mkjo@itu.dk>
2023-03-22 00:34:00 +0100

    add info button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Controls from './components/controls/Controls';
+import Info from './components/info/Info';
 
 function App() {
   return (
     <div className="App">
       <h1>Conways Game of Life</h1>
       <h2>Work in progress... 🏗️</h2>
-      <Controls className="Controls"/>
+      <Controls/>
+      <Info/>
+
     </div>
   );
 }
