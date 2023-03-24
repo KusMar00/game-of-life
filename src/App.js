c471cd004a17192f3f4d14ae1a7fca0f1be23155
Markus Johansen <mkjo@itu.dk>
2023-03-24 10:11:58 +0100

    setup canvas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Controls from './components/controls/Controls';
 import Info from './components/info/Info';
+import Board from './components/board/Board';
 
 function App() {
   return (
     <div className="App">
       <h1>Conways Game of Life</h1>
       <h2>Work in progress... 🏗️</h2>
+      <Board/>
       <Controls/>
       <Info/>
 
--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "./Board.css";
 
 const Board = () => {
-  return <div>Board</div>;
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+  }, []);
+
+  return <canvas ref={canvasRef} className="canvas" />;
 };
 
 export default Board;
